33720b4119fb5ec58a30bd6d2221e7e866563dd2
Meghashyam <shyamms17@hotmail.com>
2025-02-13 21:44:07 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="shyam",
+    author_email="shyamms100@hotmail.co.in",
+    packages=find_packages(),
+    install_requires=[],
+)
